a454aa3f00af79ec585ad45a329ef4e3b3cef63b
Felipe  Fahrion <felipefahrion@MacBook-Air-de-Felipe.local>
2022-03-29 21:24:48 -0300

    create server and client files

---
+++ GRPC/src/server/server.js
@@ -0,0 +1,22 @@
+const grpc = require("@grpc/grpc-js")
+const news = require('../../db/news')
+const gRPCPackegeDefinition = require('../config/packageDefinition')
+
+const newsProto = grpc.loadPackageDefinition(gRPCPackegeDefinition);
+
+const server = new grpc.Server();
+
+server.addService(newsProto.NewsService.service, {
+  getAllNews: (_, callback) => {
+    callback(null, news);
+  },
+});
+
+server.bindAsync(
+  'localhost:50051',
+  grpc.ServerCredentials.createInsecure(),
+  (error, port) => {
+    console.log("Server running at localhost:50051");
+    server.start();
+  }
+);
