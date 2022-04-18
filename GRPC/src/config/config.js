dfd9b1aed4e2a860fae72e6bc83035c58cd2b28f
Felipe  Fahrion <felipefahrion@MacBook-Air-de-Felipe.local>
2022-04-18 09:38:02 -0300

    fix identeation

--- GRPC/src/config/config.js
+++ GRPC/src/config/config.js
@@ -5,13 +5,13 @@ const protoLoader = require("@grpc/proto-loader");
 console.log(PROTO_PATH);
 
 const options = {
-    keepCase: true,
-    longs: String,
-    enums: String,
-    defaults: true,
-    oneofs: true,
-  };
-  
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+};
+
 const gRPCPackegeDefinition = protoLoader.loadSync(PROTO_PATH, options);
 
 module.exports = gRPCPackegeDefinition
